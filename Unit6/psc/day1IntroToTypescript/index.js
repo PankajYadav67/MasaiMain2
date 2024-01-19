b258232607f2b587448c6942bff7f500f285b8ca
PankajKumar Yadav <132429460+PankajYadav67@users.noreply.github.com>
2024-01-19 22:04:35 +0530

    typescript intro done.

---
+++ Unit6/psc/day1IntroToTypescript/index.js
@@ -0,0 +1,6 @@
+var x = 10;
+var y = 20;
+function sum(a, b) {
+    console.log(a+b);
+}
+sum(45, 8);
---
+++ Unit6/psc/day1IntroToTypescript/index.ts
@@ -0,0 +1,11 @@
+const x : number = 10 ;
+const y : string = "20";
+
+function sum (a : number, b : number){
+    return a + b;
+}
+sum(45,8);
+
+if(x==y){
+    
+}
