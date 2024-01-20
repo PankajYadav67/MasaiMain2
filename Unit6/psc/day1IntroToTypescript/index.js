b3fbc9a0e1b10f52488457025aef4f9ebbe13906
PankajKumar Yadav <132429460+PankajYadav67@users.noreply.github.com>
2024-01-20 22:46:49 +0530

    intro to typescript

--- Unit6/psc/day1IntroToTypescript/index.js
+++ Unit6/psc/day1IntroToTypescript/index.js
@@ -1,6 +1,81 @@
 var x = 10;
 var y = 20;
-function sum(a, b) {
-    console.log(a+b);
+function sum1(a, b) {
+    return a + b;
 }
-sum(45, 8);
+sum1(45, 8);
+if (x == y) {
+    var min = x - y;
+}
+;
+var a = true;
+a = false;
+// a= 1
+var b = 15;
+b = 41;
+// b = false
+var arr = [12, 65, 445];
+var arrString = ["247", "258", "836"];
+// or operator
+var arr199 = [12, "65", 445];
+//array of number | boolean
+var arrBooleanNumber = [true, 12, true, 954];
+// array of boolean
+var arrBoolean1 = [true, false, true, false];
+var arrBoolean2 = [true, false, true, false];
+// array of number | string
+var arrStringNumber = ["true", 12, "true", 954];
+// null
+var p = null;
+p = 5;
+//undefined
+var bv = undefined;
+bv = 5;
+// function 
+// argument
+function sum2(a, b) {
+    return (a + b).toString();
+}
+// optional argument
+function sum3(a, b) {
+    if (!b) {
+        return a.toString();
+    }
+    return (a + b).toString();
+}
+//default value seted
+function sum4(a, b) {
+    if (b === void 0) { b = "0"; }
+    if (!b) {
+        return a.toString();
+    }
+    return (a + b).toString();
+}
+// void -> not returning anything just do thing only
+//any -> avoid use any just similar to js idk anything kindof thing
+function sum5(a, b) {
+    if (b === void 0) { b = 0; }
+    if (!b) {
+        return a;
+    }
+    else {
+        return (a - b);
+    }
+    return (a + b);
+}
+var u = {
+    name: "ritesh",
+    age: 20,
+    place: "India",
+    number: 1122,
+    ismarried: false,
+    rollNo: 1,
+    batch: 2
+};
+var us1 = {
+    name: "ritesh",
+    age: 20,
+    place: "India",
+    number: 1122,
+    ismarried: false,
+};
--- Unit6/psc/day1IntroToTypescript/index.ts
+++ Unit6/psc/day1IntroToTypescript/index.ts
@@ -1,11 +1,154 @@
 const x : number = 10 ;
-const y : string = "20";
+const y : number = 20;
 
-function sum (a : number, b : number){
+function sum1 (a : number, b : number){
     return a + b;
 }
-sum(45,8);
+sum1(45,8);
 
 if(x==y){
-    
+    const min = x-y;
+};
+
+
+
+
+var a : boolean = true;
+a = false;
+// a= 1
+
+
+var b : number = 15;
+b =41;
+// b = false
+
+
+
+
+
+let arr : Array<number> = [12,65,445];
+const arrString : string[] = ["247","258","836"];
+
+
+
+
+// or operator
+
+let arr199 : Array<number | string> = [12,"65",445];
+
+
+
+
+//array of number | boolean
+let arrBooleanNumber: Array< number|boolean> = [true,12,true,954] ;
+
+// array of boolean
+let arrBoolean1: Array<boolean> = [true,false,true,false] ;
+let arrBoolean2: boolean[] = [true,false,true,false] ;
+
+// array of number | string
+let arrStringNumber: Array< number|string> = ["true",12,"true",954] ;
+
+
+
+// null
+
+let p : null | number = null;
+p =5 ;
+
+//undefined
+let bv : undefined | number = undefined;
+bv =5 ;
+
+
+
+
+
+// function 
+
+// argument
+
+function sum2 (a : number, b : string) : string{
+    return (a + b).toString();
+}
+
+// optional argument
+function sum3 (a : number, b?: string ) : string{
+    if(!b){
+        return a.toString();
+    }
+    return (a + b).toString();
+}
+
+//default value seted
+
+function sum4 (a : number, b: string = "0" ) : string{
+    if(!b){
+        return a.toString();
+    }
+    return (a + b).toString();
+}
+
+// void -> not returning anything just do thing only
+
+//any -> avoid use any just similar to js idk anything kindof thing
+
+function sum5 (a : number, b: number = 0 ) : any{
+    if(!b){
+        return a;
+    }else{
+        return (a- b);
+    }
+    return (a + b);
+}
+
+//onject
+
+type User= {
+    name:string;
+    age:number;
+    place:"India"| "U.S.A" | "Russia" | "Japan";
+    number:number;
+    ismarried:boolean;
+}
+type Student = {
+    rollNo:number;
+    batch:number;
+}
+
+let u : User & Student ={
+    name : "ritesh",
+    age: 20,
+    place:"India",
+    number:1122,
+    ismarried:false,
+    rollNo:1,
+    batch:2
+}
+
+
+
+//interface
+
+interface User1 {
+    name:string;
+    age:number;
+    place:"India"| "U.S.A" | "Russia" | "Japan";
+    number:number;
+    ismarried:boolean;
+}
+
+interface Students extends User1 {
+    rollNo:number;
+    batch:number;
+}
+
+let us1 :User1 ={
+    name : "ritesh",
+    age: 20,
+    place:"India",
+    number:1122,
+    ismarried:false,
+  
+
 }
